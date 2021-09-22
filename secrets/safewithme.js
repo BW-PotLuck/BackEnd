cee0570d84ec4fb64cd52d6886a0ae11dcc95918
Rodgers31 <otienor986@gmail.com>
2021-09-22 19:36:59 -0400

    remaining endpoints begins

--- secrets/safewithme.js
+++ secrets/safewithme.js
@@ -1,3 +1,4 @@
 module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || "Super secret password or something like that",
-  }
+	JWT_SECRET:
+		process.env.JW_SECRET || 'Super secret password or something like that',
+};
