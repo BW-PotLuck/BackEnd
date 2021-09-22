026a9a7c35511117e80934d11e4f83b81c1f4f68
Jesse <eoshout@Gmail.com>
2021-09-22 14:22:18 -0400

    test

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -20,16 +20,25 @@ router.post('/register', checkCredetials, checkNameExist, (req, res, next) => {
 		.catch(next);
 });
 
-router.post('/login', checkUserNameInDb, (req, res, next) => {
-	if (bcrypt.compareSync(req.body.password, req.user.password)) {
-		const token = buildToken(req.user);
-		res.json({
-			message: `Welcome, ${req.user.username}`,
-			token,
-		});
-	} else {
-		next({ status: 401, message: 'invalid credentials' });
-	}
-});
+
+router.post('/login',checkUserNameInDb, (req, res) => {
+    let { username, password } = req.body
+    Users.findBy({ username })
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = buildToken(user)
+          res.status(200).json({
+            user_id: user.user_id,
+            message: `welcome, ${username}`,
+            token: token
+          })
+        } else {
+          res.status(401).json({ message: 'invalid credentials'})
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err.message)
+      })
+})
 
 module.exports = router;
